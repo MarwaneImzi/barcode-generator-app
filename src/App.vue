49cc77b2f2cdfc8ed43f6df9c565de601fda6bee
Marwane Imzi <60143480+MarwaneImzi@users.noreply.github.com>
2023-05-23 23:30:15 +0100

    footer added

--- src/App.vue
+++ src/App.vue
@@ -71,6 +71,13 @@ const DeleteBarcodes = () => {
       </div>
     </div>
   </div>
+  <footer class="text-gray-400 bg-slate-900 body-font">
+  <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+    <p class="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2023 Marwane —
+      <a href="https://github.com/MarwaneImzi" class="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">MarwaneImzi</a>
+    </p>
+  </div>
+</footer>
 </template>
 
 <style scoped>
