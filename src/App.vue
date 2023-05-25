788c4dbc3f758de7e24618a2f1e33714c116c1a5
Marwane Imzi <60143480+MarwaneImzi@users.noreply.github.com>
2023-05-25 09:50:39 +0100

    fixed dark mode issue

--- src/App.vue
+++ src/App.vue
@@ -62,14 +62,14 @@ function wait() {
         
         <!-- Toggle list function  -->
         <div class="container mx-auto flex justify-center mb-2 noPrint">
-          <span class="mx-3 text-sm font-medium text-gray-900 dark:text-gray-300">Printing</span>
+          <span class="mx-3 text-sm font-medium text-gray-300">Printing</span>
           <label class="relative inline-flex items-center mx-5 cursor-pointer">
             <input type="checkbox" v-model="viewToggle" class="sr-only peer" checked>
             <div
               class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600">
             </div>
           </label>
-          <span class="mx-3 text-sm font-medium text-gray-900 dark:text-gray-300">Scanning</span>
+          <span class="mx-3 text-sm font-medium text-gray-300">Scanning</span>
         </div>
         <div class="noPrint border-b-2 border-gray-700 container mx-auto"></div>
         
